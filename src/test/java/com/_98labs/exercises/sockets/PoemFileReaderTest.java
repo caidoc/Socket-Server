25eeeb58e4eb34f9ffc1988654c66c62d47dbd72
unknown <michael.caidoc@98labs.com>
2024-05-02 21:36:23 -1100

    Stored Poem lines into an array list

--- src/test/java/com/_98labs/exercises/sockets/PoemFileReaderTest.java
+++ src/test/java/com/_98labs/exercises/sockets/PoemFileReaderTest.java
@@ -14,6 +14,7 @@ public class PoemFileReaderTest {
         // Initialize a PoemFileReader instance with a temporary file
         poemFileReader = new PoemFileReader();
     }
+    /*
     @Test
     public void testGetLine() throws IOException {
         // Test reading the first line
@@ -22,6 +23,25 @@ public class PoemFileReaderTest {
         assertEquals("BY MAYA ANGELOU", poemFileReader.getLine(2));
         // Test reading the third line
         assertEquals("You may write me down in history", poemFileReader.getLine(3));
+        // Test reading a non-existent line
+        assertEquals("Line not found", poemFileReader.getLine(55));
+        // Test reading a non-existent line
+        assertEquals("Line not found", poemFileReader.getLine(1000000));
+        // Test reading a non-existent line
+        assertEquals("Line not found", poemFileReader.getLine(-1000));
+        // Test reading a non-existent line
+        assertEquals("Line not found", poemFileReader.getLine(0));
+    }
+
+     */
+    @Test
+    public void testGetLine() throws IOException {
+        // Test reading the first line
+        assertEquals("One Art", poemFileReader.getLine(1));
+        // Test reading the second line
+        assertEquals("BY ELIZABETH BISHOP", poemFileReader.getLine(2));
+        // Test reading the third line
+        assertEquals("The art of losing isn’t hard to master;", poemFileReader.getLine(3));
         // Test reading a non-existent line
         assertEquals("Line not found", poemFileReader.getLine(55));
         // Test reading a non-existent line
